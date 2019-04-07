682f13804bfefd0b2d22c68c232d3e81fe8f20f7
Danylo Kazymyrov <danil.kazim.99@gmail.com>
2019-04-07 17:03:56 +0300

    add different configs for lab1 & lab2

---
+++ README.md
@@ -0,0 +1,11 @@
+# Lab1
+![Task preview](./public/lab1-preview.png)
+```bash
+mvn exec:java@lab1
+```
+
+# Lab2
+![Task preview](./public/lab2-preview.png)
+```bash
+mvn exec:java@lab2
+```
--- src/main/java/com/zulus/lab1/Main.java
+++ src/main/java/com/zulus/lab1/Main.java
@@ -1,15 +1,12 @@
-package com.zulus;
+package com.zulus.lab1;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Polygon;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
